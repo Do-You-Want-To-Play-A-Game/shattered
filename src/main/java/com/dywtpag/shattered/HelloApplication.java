04ab2b2ea3c8efa28761f2ab1eb6fd8d62881226
Adrian Butler Jr <butlerjr123@gmail.com>
2022-11-17 12:39:03 -0600

    testing

--- src/main/java/com/dywtpag/shattered/HelloApplication.java
+++ src/main/java/com/dywtpag/shattered/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application
 	{
 		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+		stage.setTitle("Testing");
 		stage.setScene(scene);
 		stage.show();
 	}
