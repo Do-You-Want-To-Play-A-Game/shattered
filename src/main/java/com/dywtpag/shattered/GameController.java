b99cc0e1a488c586fc6c7a7c48ce953a7c7288f6
Adrian Butler Jr <butlerjr123@gmail.com>
2022-11-21 17:44:10 -0600

    added class to test game and ignore menu. Added all puzzle pieces into a grid.

--- src/main/java/com/dywtpag/shattered/GameController.java
+++ src/main/java/com/dywtpag/shattered/GameController.java
@@ -4,27 +4,48 @@ import com.dywtpag.shattered.puzzle.PuzzleCreator;
 import com.dywtpag.shattered.puzzle.PuzzleNode;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-public class GameController {
+public class GameController
+{
 
-    @FXML
-    public AnchorPane main;
+	@FXML
+	public AnchorPane main;
 
-    public void makeGame() throws IOException {
-        ArrayList<PuzzleNode> puzzle = PuzzleCreator.createPuzzle(FileUploadController.getImgToChop(), 1920, 1080);
-        //todo let rows and columns be changed
+	@FXML
+	public GridPane grid;
 
-        for (PuzzleNode puzzleNode : puzzle)
-        {
-            main.getChildren().add(puzzleNode);
-        }
-    }
+	public void makeGame()
+	{
+		PuzzleNode[][] puzzle = PuzzleCreator.createPuzzle(FileUploadController.getImgToChop(), 10, 5);
+		//todo I think the rows and columns are backwards (its making 10 col 5 rows instead of 10 rows 5 col) fix
+		// this later
 
-    @FXML
-    public void initialize() throws IOException {
-        makeGame();
-    }
+		//todo let rows and columns be changed (with difficulty selection)
+
+		for (int i = 0; i < puzzle.length; i++)
+		{
+			grid.addColumn(i);
+			PuzzleNode[] puzzleNodeRow = puzzle[i];
+
+			for (int j = 0; j < puzzleNodeRow.length; j++)
+			{
+				PuzzleNode puzzleNode = puzzleNodeRow[j];
+
+				grid.addRow(j);
+				grid.add(puzzleNode, i, j);
+
+//				main.getChildren().add(puzzleNode);
+			}
+		}
+	}
+
+	@FXML
+	public void initialize() throws IOException
+	{
+		makeGame();
+	}
 }
